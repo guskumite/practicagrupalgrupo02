8cbfd6e1370b97b4746d64231ba2c0e27266961a
jrlopes <contacto.juniorlope@gmail.com>
2023-08-18 01:14:42 -0500

    add pet photo and name

--- src/components/juniorlopez.jsx
+++ src/components/juniorlopez.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import nua from '../assets/img/nua.jpg'
+const JuniorLopez = () => {
 
-const juniorlopez = () => {
-  return <div>junior</div>;
+  return (
+    <section className='container'>
+      <h1>Junior Lopez</h1>
+
+      <img className='nua' src= {nua} alt="" />
+    </section>
+  );
 };
 
-export default juniorlopez;
+export default JuniorLopez;
